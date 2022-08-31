7018864336433514972b957700255028f1ec8c04
Ali Baykal <alib@dasburo.com>
2022-08-31 13:18:45 +0200

    Init szlk application

--- packages/model/dist/Language.d.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-export declare const German = "de";
-export declare const Turkish = "tr";
-export declare const Language: z.ZodEnum<["de", "tr"]>;
-//# sourceMappingURL=Language.d.ts.map
--- packages/model/dist/Language.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Language = exports.Turkish = exports.German = void 0;
-const zod_1 = require("zod");
-exports.German = 'de';
-exports.Turkish = 'tr';
-exports.Language = zod_1.z.enum([exports.German, exports.Turkish]);
